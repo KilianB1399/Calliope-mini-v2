becfd6b4b072bfcb5456ed659b9429e4bd7654e3
KilianB1399 <kilianbogus@bbd-8.org>
2024-09-18 13:03:10 +0200

    Add files via upload

--- Rucksack alarm mit Sterung/rucksack_alarm_sender.js
+++ Rucksack alarm mit Sterung/rucksack_alarm_sender.js
@@ -1,30 +1,8 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 13.7) {
-        locked = 1
-        control.waitMicros(10000000)
-        locked = 0
-    }
+input.onButtonEvent(Button.AB, input.buttonEventValue(ButtonEvent.Down), function () {
+    radio.sendNumber(1)
+    basic.setLedColor(0x00ff00)
+    control.waitMicros(10000000)
+    basic.setLedColor(0xff0000)
 })
-function triggerbrightness2 () {
-    music.stopAllSounds()
-}
-function checkbrightness (brightness2: number) {
-    if (locked == 0) {
-        if (brightness2 >= 100) {
-            triggerbrightness()
-        }
-        if (brightness2 < 100) {
-            triggerbrightness2()
-        }
-    }
-}
-function triggerbrightness () {
-    music.playTone(988, music.beat(BeatFraction.Whole))
-}
-let brightness = 0
-let locked = 0
 radio.setGroup(39)
-basic.forever(function () {
-    brightness = input.lightLevel()
-    checkbrightness(brightness)
-})
+basic.setLedColor(0xff0000)
