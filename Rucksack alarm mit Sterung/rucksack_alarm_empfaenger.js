c4c511215c9521176fff602e42d7c9eb759fb6d1
KilianB1399 <kilianbogus@bbd-8.org>
2024-10-02 12:42:59 +0200

    Update rucksack_alarm_empfaenger.js

--- Rucksack alarm mit Sterung/rucksack_alarm_empfaenger.js
+++ Rucksack alarm mit Sterung/rucksack_alarm_empfaenger.js
@@ -1,30 +1,20 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 13.7) {
-        locked = 1
-        control.waitMicros(10000000)
-        locked = 0
+    if (receivedNumber == 13) {
+        modus = 1
+    }
+    if (receivedNumber == 12) {
+        modus = 0
     }
 })
-function triggerbrightness2 () {
-    music.stopAllSounds()
-}
-function checkbrightness (brightness2: number) {
-    if (locked == 0) {
-        if (brightness2 >= 100) {
-            triggerbrightness()
+let modus = 0
+radio.setGroup(1)
+basic.forever(function () {
+    if (modus == 0) {
+        if (input.lightLevel() <= 100) {
+            music.stopAllSounds()
         }
-        if (brightness2 < 100) {
-            triggerbrightness2()
+        if (input.lightLevel() > 100) {
+            music.playTone(932, music.beat(BeatFraction.Whole))
         }
     }
-}
-function triggerbrightness () {
-    music.playTone(988, music.beat(BeatFraction.Whole))
-}
-let brightness = 0
-let locked = 0
-radio.setGroup(39)
-basic.forever(function () {
-    brightness = input.lightLevel()
-    checkbrightness(brightness)
 })
